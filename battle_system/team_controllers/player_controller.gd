40ab1cb665e651933e64fe71e3247e0d0a2a4388
Lukie4reals <lajoplin@protonmail.com>
2026-04-08 10:22:27 -0500

    connected team_controller stuff

--- battle_system/team_controllers/player_controller.gd
+++ battle_system/team_controllers/player_controller.gd
@@ -1,4 +1,19 @@
 class_name PlayerController extends TeamController
 
+signal battler_selected_move
+
 func _ready() -> void:
 	moves_decided.emit(self)
+
+func decide_moves():
+	player_ui.temp_signal.connect(_on_temp_signal)
+
+	for battler in battler_array:
+		await battler_selected_move
+	
+	moves_decided.emit(self)
+
+func _on_temp_signal(battler_index: int, move_index: int):
+	var battler: Battler = battler_array[battler_index]
+	battler.select_move(move_index)
+	battler_selected_move.emit()
