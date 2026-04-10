521fdd00c950fb7bf7b06f50c94be857936fce36
Lukie4reals <lajoplin@protonmail.com>
2026-04-10 10:16:25 -0500

    changed signal names

--- battle_system/team_controllers/player_controller.gd
+++ battle_system/team_controllers/player_controller.gd
@@ -2,18 +2,20 @@ class_name PlayerController extends TeamController
 
 signal battler_selected_move
 
+@export var player_ui: Node2D
+
 func _ready() -> void:
 	moves_decided.emit(self)
 
 func decide_moves():
-	player_ui.temp_signal.connect(_on_temp_signal)
+	player_ui.hero_moved.connect(_on_temp_signal)
 
 	for battler in battler_array:
 		await battler_selected_move
 	
 	moves_decided.emit(self)
 
-func _on_temp_signal(battler_index: int, move_index: int):
+func _on_temp_signal(move_index: int, battler_index: int):
 	var battler: Battler = battler_array[battler_index]
 	battler.select_move(move_index)
 	battler_selected_move.emit()
--- battle_system/team_controllers/team_controller.gd
+++ battle_system/team_controllers/team_controller.gd
@@ -1,7 +1,5 @@
 class_name TeamController extends Node
 
-@export var player_ui: Node2D
-
 signal moves_decided(team: TeamController)
 
 var battler_array: Array[Battler]
