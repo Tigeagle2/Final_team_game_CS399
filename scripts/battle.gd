f48ecad81859de8694ffc378a1852511b502f95e
Caden <cadendouglas05@gmail.com>
2026-04-13 10:53:35 -0500

    worked on energy UI Stuff

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -3,7 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	call_deferred("link_battler")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+func link_battler():
+	%fight_screen_ui.hero_ui_1.battler = %hero_infight_1/Battler
+	%fight_screen_ui.hero_ui_2.battler = %hero_infight_2/Battler
+	%fight_screen_ui.hero_ui_3.battler = %hero_infight_3/Battler
+	%fight_screen_ui.hero_ui_1.setup_battler()
+	%fight_screen_ui.hero_ui_2.setup_battler()
+	%fight_screen_ui.hero_ui_3.setup_battler()
