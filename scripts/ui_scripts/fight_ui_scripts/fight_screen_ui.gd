42040d3394dfc7eca6d6a5ee82401445260d2acc
Caden <cadendouglas05@gmail.com>
2026-04-16 16:03:06 -0500

    hopefully got a verson 1 of the targeting system

--- scripts/ui_scripts/fight_ui_scripts/fight_screen_ui.gd
+++ scripts/ui_scripts/fight_ui_scripts/fight_screen_ui.gd
@@ -13,12 +13,13 @@ var hero_num_stored = null
 var target = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	retrieve_all_battlers()
+	call_deferred("retrieve_all_battlers")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 func retrieve_all_battlers():
+	await get_tree().create_timer(0.1).timeout
 	all_battlers.clear()
 	var battler_nodes = get_tree().get_nodes_in_group("enemy_battlers")
 	all_battlers.append(hero_ui_1.battler)
@@ -28,6 +29,11 @@ func retrieve_all_battlers():
 		if node is Battler:
 			all_battlers.append(node)
 	print("Successfully retrieved ", all_battlers.size(), " battlers!")
+	if all_battlers.size() == 4:
+		%target_button_enemy_2.queue_free()
+		%target_button_enemy_3.queue_free()
+	elif all_battlers.size() == 5:
+		%target_button_enemy_3.queue_free()
 func change_current_tab():
 	if %TabContainer.current_tab == 1:
 		if hero_ui_1.skill_menu_open:
@@ -40,7 +46,10 @@ func change_current_tab():
 			hero_ui_3.toggle_skill_menu()
 	if %TabContainer.current_tab == %TabContainer.get_tab_count() - 1:
 		%TabContainer.set_current_tab(0)
+		%target_button_container.hide()
 	else:
+		if %target_button_container.hidden:
+			%target_button_container.show()
 		%TabContainer.set_current_tab(%TabContainer.current_tab + 1)
 	if %TabContainer.get_current_tab_control().has_method("input_focus") && Input.get_connected_joypads().size() > 0:
 		%TabContainer.get_current_tab_control().input_focus()
@@ -59,13 +68,16 @@ func _on_button_pressed() -> void:
 func _on_hero_ui_1_hero_move(index: int, hero_num: int) -> void:
 	move_index_stored = index
 	hero_num_stored = hero_num
+	target_selection()
 	
 func _on_hero_ui_2_1_hero_move(index: int, hero_num: int) -> void:
 	move_index_stored = index
 	hero_num_stored = hero_num
+	target_selection()
 func _on_hero_ui_3_1_hero_move(index: int, hero_num: int) -> void:
 	move_index_stored = index
 	hero_num_stored = hero_num
+	target_selection()
 func target_selection():
 	var target_array
 	if move_index_stored == null || hero_num_stored == null:
@@ -89,3 +101,28 @@ func target_selection():
 	if target in target_array:
 		hero_moved.emit(move_index_stored, hero_num_stored, [target])
 		change_current_tab()
+
+
+func _on_target_button_hero_1_pressed() -> void:
+	target = all_battlers[0]
+	target_selection()
+
+func _on_target_button_hero_2_pressed() -> void:
+	target = all_battlers[1]
+	target_selection()
+
+func _on_target_button_hero_3_pressed() -> void:
+	target = all_battlers[2]
+	target_selection()
+
+func _on_target_button_enemy_1_pressed() -> void:
+	target = all_battlers[3]
+	target_selection()
+
+func _on_target_button_enemy_2_pressed() -> void:
+	target = all_battlers[4]
+	target_selection()
+
+func _on_target_button_enemy_3_pressed() -> void:
+	target = all_battlers[5]
+	target_selection()
