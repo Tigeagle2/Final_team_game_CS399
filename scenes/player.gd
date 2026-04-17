977920bdc5d88408cb2d53969bef3b1e0e8effd8
Melvin <mbenitezlopez@stu.jsu.edu>
2026-04-17 05:25:14 -0500

    added main level and assets

---
+++ scenes/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = input_direction * SPEED
+	
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
